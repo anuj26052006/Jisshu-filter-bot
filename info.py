72bbaeba11f615c25ff5089919db86df85fa5f2c
anuj26052006 <anujkumar26052006@gmail.com>
2025-01-19 11:41:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5244780439').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/deactivated_2") # ADMIN USERNAME
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1286101018').split()]
+USERNAME = environ.get('USERNAME', "https://t.me/GwAnuj") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Rashmika_allmovie_bot')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002381849087').split()]
